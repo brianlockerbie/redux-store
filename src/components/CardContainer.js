fbcf104f37f1da27219693a5ca4f5547b5a784a1
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-10-07 19:19:46 -0400

    Created CardContainer and Imported it to Hero

---
+++ src/components/CardContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CardContainer = () => {
+  return (
+    <div className="cardContainer">
+      <p>Card</p>
+    </div>
+  );
+};
+
+export default CardContainer;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import Nav from "./Nav";
 import Container from "./reusableComponents/Container";
+import CardContainer from './CardContainer';
 
 const Hero = () => {
   return (
     <section className="hero">
       <Nav />
-      <Container></Container>
+      <Container>
+          <CardContainer />
+      </Container>
     </section>
   );
 };
