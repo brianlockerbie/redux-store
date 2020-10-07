fb2c90c72cf9e21cb2190e7aeff85f1daa388511
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-10-07 19:12:12 -0400

    Update Hero.js
    Imported Nav and Container

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import Nav from "./Nav";
+import Container from "./reusableComponents/Container";
 
 const Hero = () => {
-
-    return(
-        <section className="hero">
-
-        </section>
-    )
-}
+  return (
+    <section className="hero">
+      <Nav />
+      <Container></Container>
+    </section>
+  );
+};
 
 export default Hero;
