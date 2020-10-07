a25b586eb33459feabc8e4e11c4760f9c28edaf0
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-10-07 19:44:02 -0400

    created CardInfo / imported button

---
+++ src/components/CardBg.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CardBg = () => {
+  return (
+    <div className="cardBg">
+      <img src="" alt="" />
+    </div>
+  );
+};
+
+export default CardBg;
---
+++ src/components/CardInfo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./reusableComponents";
+
+const CardInfo = () => {
+  return (
+    <div className="cardInfo">
+      <p>Price</p>
+      <Button />
+    </div>
+  );
+};
+
+export default Button;
