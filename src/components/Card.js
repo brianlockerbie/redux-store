0814aae12adc67f53021db3c1d179c699c6ec50b
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-10-07 19:25:07 -0400

    Created Card and imported it

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <>
+      <p>card</p>
+    </>
+  );
+};
+
+export default Card;
--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Card from './Card';
 
 const CardContainer = () => {
   return (
     <div className="cardContainer">
-      <p>Card</p>
+      <Card />
     </div>
   );
 };
