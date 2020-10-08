fbd7a3ab4364541e98a2f9948b70f6b7dba1fa14
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-10-08 11:28:33 -0400

    Added hiddenMenu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,21 @@
 import React from "react";
 import Container from "./reusableComponents/Container";
 
-const Nav = () => {
+const Nav = ({ hiddenMenu, setHiddenMenu, itemCount }) => {
   return (
     <nav>
       <Container>
         <h2>
           Shoes<span></span>
         </h2>
-        <div className="menuWrapper"></div>
+        <div className="menuWrapper" onClick={() => setHiddenMenu(!hiddenMenu)}>
+          <i
+            className={
+              hiddenMenu ? "fas fa-times fa-lg" : "fas fa-shopping-cart fa-lg"
+            }
+          ></i>
+          <p>{itemCount > 0 && itemCount}</p>
+        </div>
       </Container>
     </nav>
   );
